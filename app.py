b901d45e71f7fd283cf31fb7cbafeaa989f757db
pkeil7 <paul.keil@hereon.de>
2026-06-01 15:23:44 +0200

    updated requirements

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import os
 from pathlib import Path
 from urllib.request import Request, urlopen
 
+#import matplotlib
+#matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
